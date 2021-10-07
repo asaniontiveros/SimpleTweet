73dfb61c7b96d82c5db0fd63de2c4c60b93721fc
asaniontiveros <janeontiveros16@gmail.com>
2021-10-07 18:42:52 -0500

    Tweet!

--- SimpleTweet/app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ SimpleTweet/app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -3,16 +3,21 @@ package com.codepath.apps.restclienttemplate.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Tweet {
+
 
     public String body;
     public String created_at;
     public User user;
 
+    // empty constructer
+    public Tweet(){}
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
         Tweet tweet = new Tweet();
         tweet.body = jsonObject.getString("text");
